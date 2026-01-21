064f862fdf9710740e878c2ab9f5a13911f161b9
Divesh1067 <diveshgawande1@gmail.com>
2026-01-21 09:51:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello Codingwale Vaijapur')
+  res.send('This is my node js project')
 })
 
 app.listen(port,'0.0.0.0', () => {
